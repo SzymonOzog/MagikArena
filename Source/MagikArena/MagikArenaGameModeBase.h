9689d6b851a344038f6ad0e997c289dc36a21b7b
Szymon Ozog <szymon.ozog@gmail.com>
2020-10-05 10:28:51 +0200

    Initial commit

---
+++ Source/MagikArena/MagikArenaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MagikArenaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGIKARENA_API AMagikArenaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
