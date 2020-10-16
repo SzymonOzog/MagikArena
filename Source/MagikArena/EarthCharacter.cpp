bd84f071d2f933f926cc7e56e4167df29b1ecfd1
Szymon Ozog <szymon.ozog@gmail.com>
2020-10-16 08:55:23 +0200

    Added EarthCharacter Class

---
+++ Source/MagikArena/EarthCharacter.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EarthCharacter.h"
+
+
+
+#include "TimerManager.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void AEarthCharacter::SetupUndergroundInput(UInputComponent* PlayerInputComponent)
+{
+    PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ABaseCharacter::MoveForward);
+    PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ABaseCharacter::MoveRight);
+    PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APawn::AddControllerPitchInput);
+    PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &APawn::AddControllerYawInput);
+}
+
+void AEarthCharacter::MovementAbilityImplementation()
+{
+    if(!GetCharacterMovement()->IsFalling())
+    {
+        SetActorHiddenInGame(true);
+        const FName CapsuleCollision = GetCapsuleComponent()->GetCollisionProfileName();
+        const FName MeshCollision = GetMesh()->GetCollisionProfileName();
+        GetCapsuleComponent()->SetCollisionProfileName(FName("Ghost"));
+        GetMesh()->SetCollisionProfileName(FName("Ghost"));
+        InputComponent->ClearActionBindings();
+        SetupUndergroundInput(InputComponent);
+
+        FTimerHandle Handle;
+        GetWorldTimerManager().SetTimer(Handle,
+            [this, CapsuleCollision, MeshCollision]()
+            {
+                GetCapsuleComponent()->SetCollisionProfileName(CapsuleCollision);
+                GetMesh()->SetCollisionProfileName(MeshCollision);
+                SetActorHiddenInGame(false);
+                SetupPlayerInputComponent(InputComponent);
+            }, HideUndergroundDuration, false);
+    }
+}
---
+++ Source/MagikArena/EarthCharacter.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "EarthCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGIKARENA_API AEarthCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+public:
+	void SetupUndergroundInput(UInputComponent* PlayerInputComponent);	
+private:
+	virtual void MovementAbilityImplementation() override;
+	UPROPERTY(EditAnywhere)
+	float HideUndergroundDuration = 1.5f;
+};
